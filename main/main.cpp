fafec3ed9a6839a9318f76d042165b36aa55809f
tanergungor <tanergungor@yahoo.com>
2016-06-10 18:26:05 +0300

    adding comments

--- main/main.cpp
+++ main/main.cpp
@@ -7,29 +7,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// The kernel size for the dilation
+#define DILATION_SIZE 10
+// The kernel size for the erosion
+#define EROSION_SIZE  18
+
 using namespace cv;
 using namespace std;
 
 int main( int argc, char** argv )
 {
-    /*
-    if( argc != 2)
+    if(argc != 2)
     {
-     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+     cout << "Usage: main.exe [IMAGE URL]" << endl;
      return -1;
     }
-    */
-
-    const char * path = "images/03.jpg";
-
 
     // Read the image
-    Mat src = imread(path, IMREAD_COLOR);
+    Mat src = imread(argv[1], IMREAD_COLOR);
 
     // Check for invalid input
-    if(! src.data )
+    if(!src.data)
     {
-        cout <<  "Could not open or find the image" << std::endl ;
+        cout << argv[1] << endl ;
+        cout << "Could not open or find the image" << endl ;
         return -1;
     }
 
@@ -48,115 +49,71 @@ int main( int argc, char** argv )
     // Apply threshold operation to discard the objects whose color are not white
     threshold( src_gray, src_binary, 205, 255, THRESH_BINARY );
 
+    // Create the dilation imaging
+    // more information --> http://homepages.inf.ed.ac.uk/rbf/HIPR2/dilate.htm
+    Mat src_dilation;
+    // Create the dilation kernel
+    Mat dilation_element = getStructuringElement(MORPH_ELLIPSE, Size(2 * DILATION_SIZE, 2 * DILATION_SIZE), Point(DILATION_SIZE, DILATION_SIZE));
+    // Dilate the binary image
+    dilate(src_binary, src_dilation, dilation_element);
 
+    Mat labels, stats, centroids;
 
+    // Get the connected components and stats
+    int label_size = connectedComponentsWithStats(src_dilation, labels, stats, centroids);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    Mat src_opening;
-    double dilation_size = 10;
-    Mat dilation_element = getStructuringElement(MORPH_ELLIPSE, Size(2 * dilation_size, 2 * dilation_size), Point(dilation_size, dilation_size));
-    dilate(src_binary, src_opening, dilation_element);
-
-    // delete the component in the border
-    // Get connected components and stats
-    Mat labels, stats, centroids, only;
-
-    int nLabels = connectedComponentsWithStats(src_opening, labels, stats, centroids);
-
-    // Statistics
-    // cout << "Show statistics and centroids:" << endl;
-    // cout << "stats:" << endl << "(left,top,width,height,area)" << endl << stats << endl << endl;
-    // cout << "centroids:" << endl << "(x, y)" << endl << centroids << endl << endl;
-
-    for(int i = 1; i < nLabels; i++)
+    // Suppress the structures connected to image border
+    for(int i = 1; i < label_size; i++)
     {
+        // Check if the blob is connected to image border
         if(stats.at<int>(i, CC_STAT_LEFT) == 0 ||
            stats.at<int>(i, CC_STAT_TOP)  == 0 ||
-           stats.at<int>(i, CC_STAT_LEFT) + stats.at<int>(i, CC_STAT_WIDTH ) == src_opening.size().width  ||
-           stats.at<int>(i, CC_STAT_TOP)  + stats.at<int>(i, CC_STAT_HEIGHT) == src_opening.size().height)
+           stats.at<int>(i, CC_STAT_LEFT) + stats.at<int>(i, CC_STAT_WIDTH ) == src_dilation.size().width  ||
+           stats.at<int>(i, CC_STAT_TOP)  + stats.at<int>(i, CC_STAT_HEIGHT) == src_dilation.size().height)
         {
-            compare(labels, i, only, CMP_EQ);
-
-            src_opening = src_opening - only;
+            // Create the blob
+            Mat label;
+            // Extract the current blob from the image
+            compare(labels, i, label, CMP_EQ);
+            // Delete the current blob from the image, because it is connected to the border
+            src_dilation = src_dilation - label;
         }
     }
 
-    Mat src_closing;
-    double erosion_size = 18;
-    Mat erosion_element = getStructuringElement(MORPH_ELLIPSE, Size(2 * erosion_size, 2 * erosion_size), Point(erosion_size, erosion_size));
-    erode(src_opening, src_closing, erosion_element);
+    // Create the erosion imaging
+    // more information --> http://homepages.inf.ed.ac.uk/rbf/HIPR2/erode.htm
+    Mat src_erosion;
+    // Create the erosion kernel
+    Mat erosion_element = getStructuringElement(MORPH_ELLIPSE, Size(2 * EROSION_SIZE, 2 * EROSION_SIZE), Point(EROSION_SIZE, EROSION_SIZE));
+    // Erode the binary image
+    erode(src_dilation, src_erosion, erosion_element);
 
+    // Create a vector that holds contours or blobs
+    vector<vector<Point>> contours;
 
+    // WARNING::: tune parameters
+    // Find the contours or blobs
+    findContours(src_erosion, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // ONLY ONE BLOB
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-
-    // Find contours
-    findContours(src_closing, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-    if(contours.size() != 0)
+    // WARNING::: MAYBE ORE THAN ONE BLOB CAN BE DETECTED!!!
+    // Check if blob is detected
+    if(contours.size() > 0)
     {
         // Get the moments
         Moments mu = moments(contours[0], false);
 
         // Get the mass centers:
-        Point2f mc = Point2f(mu.m10/mu.m00 , mu.m01/mu.m00);
+        Point2f mc((float)(mu.m10/mu.m00), (float)(mu.m01/mu.m00));
 
         // Draw the center of mass
         circle(src, mc, 5, Scalar(255, 0, 0), -1);
     }
 
-
-
-
-
-
-
-
-
-
     // Create a window for display
-    namedWindow("Output Image - 4", WINDOW_AUTOSIZE);
+    namedWindow("Output Image", WINDOW_AUTOSIZE);
 
     // Show our image inside it
-    imshow("Output Image - 4", src);
+    imshow("Output Image", src);
 
     imwrite("images/output.jpg", src);
 
